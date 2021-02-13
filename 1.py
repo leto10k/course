fb27290a1a602fb945d5df00a84a4d3df8839a5d
Artem <chernyshovaa@bk.ru>
2021-02-13 17:51:32 +0300

    'Fris'

---
+++ 1.py
@@ -0,0 +1,23 @@
+def discounted(price, discount):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    if discount >= 100:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price*discount/100
+    print(price_with_discount)
+
+discounted(100,5)
+
+def get_summ(one, two, delimiter = '&'):
+    return(f'{one}{delimiter}{two}')
+
+text = get_summ('Learn','python')
+print(text.upper())
+
+def format_price(price):
+    price = int(price)
+    return f'Цена: {price} руб.'
+
+a = format_price(56.24)
+print(a)
